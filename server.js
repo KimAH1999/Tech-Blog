889ef3831169f097ab08aa8e42a8d0c2230b7b6a
Kimberly Aguilar Hermoso <kimaguilar2017@gmail.com>
2022-01-27 16:42:45 -0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connections');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3307;
 
 const hbs = exphbs.create({ helpers });
 
