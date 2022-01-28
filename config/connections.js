74a8ba61763a08112f4ecce82910ae6f761d8312
Kimberly Aguilar Hermoso <kimaguilar2017@gmail.com>
2022-01-27 16:42:10 -0800

    Update connections.js

--- config/connections.js
+++ config/connections.js
@@ -11,9 +11,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      port: 3306
+      host:'localhost',
+      dialect:'mysql',
+      port: 3307
     }
   );
 }
